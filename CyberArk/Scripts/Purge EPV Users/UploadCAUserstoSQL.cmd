605d4db7ff5fe65d55257a0a52918dc8d13c2352
abs201301 <abs201301@gmail.com>
2025-09-04 14:28:05 +0100

    Update UploadCAUserstoSQL.cmd

--- CyberArk/Scripts/Purge EPV Users/UploadCAUserstoSQL.cmd
+++ CyberArk/Scripts/Purge EPV Users/UploadCAUserstoSQL.cmd
@@ -1,3 +1,3 @@
-ExportVaultData \LogFile=UploadCAUsersToSQL.log \ContinueOnErrors \Target=MSSQL \DBServerName="cybSQLServer\SQLInstance,51433" \UsersList \enabletrace=No
+ExportVaultData \LogFile=UploadCAUsersToSQL.log \ContinueOnErrors \Target=MSSQL \DBServerName="CybDatabaseServer\SQLInstance,51433" \UsersList \enabletrace=No
 
              
